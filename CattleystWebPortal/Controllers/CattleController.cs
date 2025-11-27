e16038426061145e96d628a360380b2dcc1dcbcd
Yamil Font <Yamil@imfont.com>
2025-11-26 21:54:56 -0700

    Added cattle section in portal

---
+++ CattleystWebPortal/Controllers/CattleController.cs
@@ -0,0 +1,43 @@
+﻿using CattleystData.Models;
+using CattleystData.Models.Enums;
+using CattleystWebPortal.Interfaces;
+using CattleystWebPortal.Models.Apis;
+using CattleystWebPortal.ViewModels.Cattles;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
+
+namespace CattleystWebPortal.Controllers
+{
+    [Route("Cattle")]
+    public class CattleController : Controller
+    {
+        private readonly ILogger<CattleController> _logger;
+        private readonly IApiService _apiService;
+
+        public CattleController(ILogger<CattleController> logger,
+            IApiService apiService)
+        {
+            _logger = logger;
+            _apiService = apiService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Index()
+        {
+            CattleIndexViewModel model = new CattleIndexViewModel();
+            ApiResult<IEnumerable<Location>> result = await _apiService.GetAsync<IEnumerable<Location>>("location/list");
+            if (result.IsSuccess &&
+                result.Data != null)
+            {
+                model.LocationListItems = result.Data.Select(x => new SelectListItem()
+                {
+                    Text = x.LocationName,
+                    Value = x.LocationId.ToString()
+                }).ToList();
+            }
+            return View(model);
+        }
+    }
+}
---
+++ CattleystWebPortal/ViewModels/Cattles/CattleIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CattleystData.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CattleystWebPortal.ViewModels.Cattles
+{
+    public class CattleIndexViewModel
+    {
+        public int? LocationId { get; set; }
+        public List<SelectListItem> LocationListItems { get; set; } = [];
+
+    }
+}
